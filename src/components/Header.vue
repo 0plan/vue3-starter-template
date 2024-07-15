315c49216310f1c68876b04fa73be7baa043d4dd
Lee, Chungho <chlee125@gmail.com>
2024-07-15 15:24:33 +0900

    fix: header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,19 +12,24 @@ async function toggleLocales() {
 
 <template>
   <header className="fixed top-0 z-50 flex w-full flex-wrap justify-start py-4 text-sm backdrop-blur-sm">
-    <nav className="mx-auto flex w-full max-w-[85rem] items-center justify-between px-4">
-      <div>
-        <div i="carbon-menu" />
+    <nav className="mx-auto flex w-full max-w-[85rem] items-center px-4 justify-between">
+      <div className="flex gap-4">
+        <div i="carbon-menu" class="block md:hidden" />
+        <RouterLink to="/" class-name="basis-full text-left text-xl font-semibold">
+          <span className="font-bold sm:inline-block">🎲
+            Zero Plan Vite Vue3 Tailwind Starter</span>
+        </RouterLink>
       </div>
-      <RouterLink to="/" class-name="basis-full text-left text-xl font-semibold">
-        <span className="font-bold sm:inline-block">🎲 Zero Plan Blog</span>
-      </RouterLink>
-
-      <div className="flex items-center justify-end">
-        <Button variant="ghost" @click="toggleDark()">
+      <div className="flex items-center">
+        <div class="mr-4 hidden md:block">
+          <RouterLink to="/example">
+            Example
+          </RouterLink>
+        </div>
+        <Button variant="ghost" size="icon" @click="toggleDark()">
           <div i="carbon-sun dark:carbon-moon" />
         </Button>
-        <Button variant="ghost" @click="toggleLocales()">
+        <Button variant="ghost" size="icon" @click="toggleLocales()">
           <div i="carbon-globe" />
         </button>
       </div>
